facdebaad0d54d7da3d50d9b9e58a6db10ff04eb
Afton Downey <aaike001@gmail.com>
2016-03-25 09:43:09 -0700

    add food class

--- app/app.component.js
+++ app/app.component.js
@@ -11,7 +11,7 @@ System.register(['angular2/core'], function(exports_1, context_1) {
         if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
     };
     var core_1;
-    var AppComponent;
+    var AppComponent, Food;
     return {
         setters:[
             function (core_1_1) {
@@ -20,17 +20,31 @@ System.register(['angular2/core'], function(exports_1, context_1) {
         execute: function() {
             AppComponent = (function () {
                 function AppComponent() {
+                    this.foods = [
+                        new Food("pizza", "It had veggies.", 500, 0),
+                        new Food("salad", "no dressing", 250, 0),
+                    ];
                 }
                 AppComponent = __decorate([
                     core_1.Component({
                         selector: 'my-app',
-                        template: "\n  <div class=\"container\">\n    <h1>Food Tracker</h1>\n  </div>\n  "
+                        template: "\n  <div class=\"container\">\n    <h1>Food Tracker</h1>\n    <h3>{{ food.name }}</h3>\n  </div>\n  "
                     }), 
                     __metadata('design:paramtypes', [])
                 ], AppComponent);
                 return AppComponent;
             }());
             exports_1("AppComponent", AppComponent);
+            Food = (function () {
+                function Food(name, details, calories, id) {
+                    this.name = name;
+                    this.details = details;
+                    this.calories = calories;
+                    this.id = id;
+                }
+                return Food;
+            }());
+            exports_1("Food", Food);
         }
     }
 });
--- app/app.component.ts
+++ app/app.component.ts
@@ -5,10 +5,23 @@ import { Component } from 'angular2/core';
   template: `
   <div class="container">
     <h1>Food Tracker</h1>
+    <h3>{{ food.name }}</h3>
   </div>
   `
 })
 
 export class AppComponent {
+  public foods: Food[];
+  constructor(){
+    this.foods = [
+      new Food("pizza", "It had veggies.", 500, 0),
+      new Food("salad", "no dressing", 250, 0),
+    ];
+  }
+}
 
+export class Food {
+  constructor(public name: string, public details: string, public calories: number, public id: number){
+
+  }
 }
