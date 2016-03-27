71bfb64ef6b3973e7630d1f82962a0d3db290bba
Afton Downey <aaike001@gmail.com>
2016-03-27 14:52:30 -0700

    style html

--- app/new-food.component.ts
+++ app/new-food.component.ts
@@ -6,28 +6,27 @@ import {Food} from './food.model';
   outputs: ['onSubmitNewFood'],
   template: `
     <div class="col-sm-6">
-      <h3>Add New Food</h3>
+      <h3>Add Food</h3>
       <input placeholder="Name" #newName><br>
       <input placeholder="Details" #newDetails><br>
-      <input placeholder="Calories" #newCalories><br>
+      <input type="number" placeholder="Calories" #newCalories><br>
       <button (click)="addFood(newName, newDetails, newCalories)">Add</button>
     </div>
   `
 })
 export class NewFoodComponent {
-  public onSubmitNewFood: EventEmitter<String[]>;
-  public newFood: String[];
+  public onSubmitNewFood: EventEmitter<Food>;
   constructor() {
     this.onSubmitNewFood = new EventEmitter();
   }
-  addFood(name: HTMLInputElement, details: HTMLInputElement, calories: HTMLInputElement) {
-    this.newFood = [
-      name.value,
-      details.value,
-      calories.value];
-    this.onSubmitNewFood.emit(this.newFood);
-    name.value = "";
-    details.value = "";
-    calories.value = "";
+  addFood(userName: HTMLInputElement, userDetails: HTMLInputElement, userCalories: HTMLInputElement) {
+    var name = userName.value;
+    var details = userDetails.value;
+    var calories = parseInt(userCalories.value);
+    var newFood = new Food(name, details, calories);
+    this.onSubmitNewFood.emit(newFood);
+    userName.value = "";
+    userDetails.value = "";
+    userCalories.value = "";
   }
 }
